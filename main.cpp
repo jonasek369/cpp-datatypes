5740a7628c2b4314e5d8b40a508f4ee995c516f2
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2022-11-20 21:48:30 +0100

    C++ class

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+template <typename T>
+struct Node{
+    Node(T p_value) {
+        value = p_value;
+        next = NULL;
+    }
+
+    Node() {
+        value = NULL;
+        next = NULL;
+    }
+
+    T value;
+    Node<T>* next;
+};
+
+template <typename T>
+class LinkedList
+{
+public:
+    LinkedList(){}
+
+    // push to back
+    void push(T value)
+    {
+        if (!tail && !next) {
+            next = new Node<T>(value);
+            tail = next;
+        }else
+        {
+            tail->next = new Node<T>(value);
+            tail = tail->next;
+        }
+        size++;
+    }
+
+    // pop node at index
+    void pop(int index)
+    {
+        if (!next)
+            return;
+        if (index == 0)
+        {
+            Node<T>* temp = next->next;
+            delete next;
+            next = temp;
+        } else if(get(index) == tail)
+        { 
+            Node<T>* pnode = get(index - 1);
+            delete pnode->next;
+            pnode->next = NULL;
+        }
+        else {
+            Node<T>* left_node = get(index - 1);
+            Node<T>* mid_node = left_node->next;
+            Node<T>* right_node = mid_node->next;
+            delete mid_node;
+            left_node->next = right_node;
+        }
+    }
+
+    //get node at index
+    Node<T>* get(int index)
+    {
+        Node<T>* node = next;
+        int counter = 0;
+        while (node && counter != index)
+        {
+            node = node->next;
+            counter++;
+        }
+        if (counter != index || !node)
+        {
+            return NULL;
+        }
+        return node;
+    }
+
+    void print_chain()
+    {
+        Node<T>* node = next;
+        while (node)
+        {
+            std::cout << node->value << " ";
+            node = node->next;
+        }
+        std::cout << "\n";
+    }
+
+private:
+    Node<T>* next = NULL;
+    Node<T>* tail = next;
+    int size= 0;
+};
+
+
+int main()
+{
+    LinkedList<int> test;
+    test.push(12);
+    test.push(32);
+    test.push(82);
+    test.push(14);
+    test.pop(0);
+    test.print_chain();
+    std::cout << test.get(3) << "\n";
+}
